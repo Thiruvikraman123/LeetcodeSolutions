16e0e893044a86ba24653669eb94c92a99477961
Thiruvikraman123 <146513380+Thiruvikraman123@users.noreply.github.com>
2026-04-17 20:15:25 +0530

    Time: 44 ms (34.02%), Space: 46.8 MB (85.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,11 +6,11 @@ class Solution {
             {
                 if(nums[i]+nums[j]==target)
                 {
-                    return new int[]{i,j};
+                    return new int[] {i,j};
                 }
             }
         }
-        return new int[]{-1,1};
+        return new int[] {-1,1};
         
     }
 }
